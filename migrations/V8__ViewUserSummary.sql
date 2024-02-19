6e2aad33e9bd5a134f724131cebf6abc49594911
chandni-kha <Chandni.Khan@bbd.co.za>
2024-02-20 01:36:33 +0530

    viewUserSummary

--- migrations/V8__ViewUserSummary.sql
+++ migrations/V8__ViewUserSummary.sql
@@ -3,7 +3,13 @@ SELECT
     e.user_id,
     dbo.GetTotalExpense(e.user_id) AS totalExpense,
     dbo.GetTotalIncome(e.user_id) AS totalIncome,
-    dbo.GetTotalBudget(e.user_id) AS totalBudget
+    dbo.GetTotalBudget(e.user_id) AS totalBudget,
+    CASE
+        WHEN dbo.GetTotalExpense(e.user_id) <= dbo.GetTotalIncome(e.user_id)
+        AND dbo.GetTotalBudget(e.user_id) <= dbo.GetTotalIncome(e.user_id)
+             AND dbo.GetTotalBudget(e.user_id) >= dbo.GetTotalExpense(e.user_id) THEN 'Achieved'
+        ELSE 'Not Achieved'
+    END AS Status
 FROM
     Expense e
 GROUP BY
