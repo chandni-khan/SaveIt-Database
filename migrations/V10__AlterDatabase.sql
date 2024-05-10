bef6d8f7802509b932d908943788641156e78c28
chandni-khan <159228664+chandni-khan@users.noreply.github.com>
2024-05-10 10:41:47 +0530

    alter database

--- migrations/V10__AlterDatabase.sql
+++ migrations/V10__AlterDatabase.sql
@@ -10,50 +10,50 @@ CREATE TABLE [User] (
 ---Expense table
 CREATE TABLE Expense (
    expense_id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-   expense_category VARCHAR(50) NOT NULL,
    expense_description VARCHAR(255),
    spend_date DATE NOT NULL,
-   amount_spended DECIMAL(18,2) NOT NULL,
-   user_id INT FOREIGN KEY REFERENCES [User](user_id)
+   amount_spended MONEY NOT NULL, -- Specified data type as MONEY
+   user_id INT FOREIGN KEY REFERENCES [Users](user_id),
+   expense_category_id INT FOREIGN KEY REFERENCES [ExpenseCategory](expense_category_id)
 );
 
  CREATE TABLE ExpenseCategory (
    expense_category_id INT PRIMARY KEY,
    expense_category_name VARCHAR(50) NOT NULL,
-   expense_category_description VARCHAR(255),
+   expense_category_desc VARCHAR(255),
 );
  
 --Income table
 CREATE TABLE Income (
    income_id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-   income_category VARCHAR(50),
    income_description VARCHAR(255),
-   income DECIMAL(18,2) NOT NULL,
+   income MONEY NOT NULL,
    income_date DATE NOT NULL,
-   user_id INT FOREIGN KEY REFERENCES [User](user_id)
+   user_id INT FOREIGN KEY REFERENCES [Users](user_id),
+   income_category_id INT FOREIGN KEY REFERENCES [IncomeCategory](income_category_id)
 );
  
 CREATE TABLE IncomeCategory (
     income_category_id INT PRIMARY KEY,
     income_category_name VARCHAR(50) NOT NULL,
-    income_category_description VARCHAR(255)
+    income_category_desc VARCHAR(255)
 );
 
 --Budget table
 CREATE TABLE Budget (
    budget_id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-   budget_category VARCHAR(50) NOT NULL,
-   amount DECIMAL(18,2) NOT NULL,
+   amount MONEY NOT NULL,
    start_date DATE NOT NULL,
    end_date DATE NOT NULL,
    budget_description VARCHAR(255),
-   user_id INT FOREIGN KEY REFERENCES [User](user_id)
+   user_id INT FOREIGN KEY REFERENCES [Users](user_id),
+   budget_category_id INT FOREIGN KEY REFERENCES [BudgetCategory](budget_category_id)
 );
  
  CREATE TABLE BudgetCategory (
     budget_category_id INT PRIMARY KEY,
     budget_category_name VARCHAR(50) NOT NULL,
-    budget_category_description VARCHAR(255),
+    budget_category_desc VARCHAR(255),
 );
 
 --Goal table
